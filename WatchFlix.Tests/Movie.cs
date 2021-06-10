7824e565a5d7b180e36cbef31ed080f997c65992
Jimmy <jimmy.mattsson@schotte.se>
2021-06-10 20:59:20 +0200

    First api call test

---
+++ WatchFlix.Tests/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using Watchflix.Data;
+
+namespace WatchFlix.Tests
+{
+    [TestFixture]
+    public class Movie
+    {
+        [Test]
+        public async Task ShouldListMovies()
+        {
+            var movieSerivce = new MovieService();
+            var movies = await movieSerivce.List();
+            
+            Assert.IsNotEmpty(movies);
+            
+        }
+    }
+}
---
+++ Watchflix/Data/MovieService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TMDbLib.Client;
+using TMDbLib.Objects.Search;
+using TMDbLib.Objects.Trending;
+
+namespace Watchflix.Data
+{
+    public class MovieService
+    {
+        public async Task<IEnumerable<SearchMovie>> List()
+        {
+            var client = new TMDbClient("9d9273dd0511107b21baa2cb13b70181");
+            var movies = await client.GetTrendingMoviesAsync(TimeWindow.Day);
+            return movies.Results;
+        }
+    }
+}
